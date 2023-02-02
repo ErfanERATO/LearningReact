5bbb766b07cb0d5752eca47f1a08f3a2e14edec1
Erfan gharche beydokhti <erfan.erato.79@gmail.com>
2023-02-02 19:39:12 +0330

    complite delete

--- src/components/Counter/ShowArrayDelete.jsx
+++ src/components/Counter/ShowArrayDelete.jsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import { Form, ListGroup } from "react-bootstrap";
 
 const ShowArrayDelete = () => {
+
   const [firstName, setfirstName] = useState("");
   const [lastName, setlastName] = useState("");
   const [age, setAge] = useState(0);
@@ -52,10 +53,11 @@ const ShowArrayDelete = () => {
           <li>{a.firstName}</li>
           <li>{a.lastName}</li>
           <li>{a.age}</li>
-          <button onClick={() => deleteHandler(a)}>delete</button>
+          <button onClick={() => deleteHandler(count)}>delete</button>
         </div>
       ))}
     </div>
+   
   );
 };
 
